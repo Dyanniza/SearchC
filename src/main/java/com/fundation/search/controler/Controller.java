c0f8547ed6d4392d7f02bf23fb5cf0d8f88dc90f
Zeila Benavidez <zeilitav@gmail.com>
2018-10-22 08:29:37 -0400

    Removing extra files Adding only controller baseline and main  classes

--- src/main/java/com/fundation/search/controler/Controller.java
+++ src/main/java/com/fundation/search/controler/Controller.java
@@ -5,12 +5,9 @@
  *
  *  This software is the confidential and proprietary information of  Jalasoft, ("Confidential Information").  You shall not
  * disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement you entered into with Jalasoft.
- *
- *
- *
  */
 
-package com.fundation.search.controler;
+package main.java.com.fundation.search.controler;
 
 public class Controller
 {
@@ -20,31 +17,43 @@ public class Controller
     public Controller()
     {
         /*Instance the view and the search model*/
-        view= new View();
+        view= new ViewSearch();
         search= new Search();
 
         /*this is when the action Listener is called for get the data from the view*/
         view.getPanel().getButton().addActionListener(e-> getCriteriaView());// getCriteriaView is the name of the button
-
     }
 
     //This is what the View will return when the button is pressed
     private void getCriteriaView()
     {
         //System.out.print("Hello");
+        //obtaining what the view is passing for the parameters
+        String path =view.getPanel().getPath();  //path
+        String fileName =view.getPanel().getFileName(); //filename
+       // String ext =view.getPanel().getExt();//extension
+       // Date dateA = view.getPanel().getDateA();//creationdate
 
-        String path =View.getPanel().getPath();  //this is what the view is given me path
-        String fileName =View.getPanel().getFileName(); //this is what the view is given me filename
-
-        List<FileS> filesR=search.searchfiles(path,fileName); //this is what the model is returned
+        //This is the object that will pass to the Model the data
+        List<FileResul> filesR=search.initSearch(path,fileName);
+        //List<FileResult> filesR=search.initSearch(path,fileName, ext, dateA); //this is what the model is returned// sobrecargar el constructor
 
         //this is what the controller made for iterate all what the model is passed and set the result table
 
-        for(files f: filesR)
+        //add un clean default model
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        while( model.getRowCount() > 0 )
         {
-            System.out.println("path:"+ f.getpath+"; filename="+ f.getpath());
+            model.removeRow(0);
         }
 
-
+        //this cycle will go throw the objects returned by the model
+        for(FileResult f: filesR)
+        {
+            this.view.getPanel().getTable().addRow(new Object [] {f.getPath(),f.getFileName()}); // this is for return the added rows in the table the values wll return.
+            //System.out.println("path:"+ f.getPath()+"; filename="+ f.getFileName());
+        }
     }
+
 }
+
